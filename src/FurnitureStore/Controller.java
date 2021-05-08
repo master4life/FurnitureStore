7ff9a6f1e96256589f5268dc3857d7ecb36ed9f9
marin <marinhincu1999@gmail.com>
2021-05-08 13:40:34 +0200

    Switching Scenes Test

--- src/FurnitureStore/Controller.java
+++ src/FurnitureStore/Controller.java
@@ -1,4 +1,31 @@
 package FurnitureStore;
 
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Controller {
+
+    @FXML
+    void loginButtonOnAction(ActionEvent event) {
+        try {
+            Parent loginParent = FXMLLoader.load(getClass().getResource("login.fxml"));
+            Scene loginScene = new Scene(loginParent);
+            Stage loginStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            loginStage.setScene(loginScene);
+            loginStage.show();
+        } catch (IOException e) {
+            System.out.println("error");
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/FurnitureStore/Main.java
+++ src/FurnitureStore/Main.java
@@ -17,7 +17,11 @@ public class Main extends Application {
     }
 
 
+
     public static void main(String[] args) {
+
+        //System.out.println(System.getProperties());
+
         launch(args);
     }
 }
