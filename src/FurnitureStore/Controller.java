19ccf0bc6390386d2e3c2f839f9426809f4b2a71
marin <marinhincu1999@gmail.com>
2021-05-10 20:43:30 +0200

    Small modifications

--- src/FurnitureStore/Controller.java
+++ src/FurnitureStore/Controller.java
@@ -10,6 +10,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Controller {
@@ -17,7 +18,7 @@ public class Controller {
     @FXML
     void loginButtonOnAction(ActionEvent event) {
         try {
-            Parent loginParent = FXMLLoader.load(getClass().getResource("login.fxml"));
+            Parent loginParent = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("login.fxml")));
             Scene loginScene = new Scene(loginParent);
             Stage loginStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
             loginStage.setScene(loginScene);
@@ -28,6 +29,12 @@ public class Controller {
         }
     }
 
+    @FXML
+    void quitButtonOnAction(ActionEvent event) {
+        final Node source = (Node) event.getSource();
+        final Stage stage = (Stage) source.getScene().getWindow();
+        stage.close();
+    }
 
 
 }
