cea7f6a8e85ff6d0895197d7d87bb1e107cb3686
marin <marinhincu1999@gmail.com>
2021-05-10 19:07:24 +0200

    Modified Welcome Scene

--- src/FurnitureStore/Main.java
+++ src/FurnitureStore/Main.java
@@ -12,7 +12,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 700, 275));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
