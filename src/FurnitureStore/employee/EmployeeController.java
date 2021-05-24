083ae0bed2219b3494bf67cbb5d8e93a9e8d2635
alllexander1 <alexanderatanassov7@gmail.com>
2021-05-24 21:39:12 +0200

    Added sign up, Import product table into tableview for employees

--- src/FurnitureStore/employee/EmployeeController.java
+++ src/FurnitureStore/employee/EmployeeController.java
@@ -1,6 +1,10 @@
 package FurnitureStore.employee;
 
 import FurnitureStore.base.Controller;
+import FurnitureStore.base.DBController;
+import FurnitureStore.models.ProductModel;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,15 +13,43 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
-public class EmployeeController{
+public class EmployeeController implements Initializable{
+
+    @FXML
+    private TableView<ProductModel> table;
+
+    @FXML
+    private TableColumn<ProductModel, Integer> colID;
+
+    @FXML
+    private TableColumn<ProductModel, String> TypeCol;
+
+    @FXML
+    private TableColumn<ProductModel, String> materialCol;
+
+    @FXML
+    private TableColumn<ProductModel, Double> sizeCol;
+
+    @FXML
+    private TableColumn<ProductModel, String> descCol;
+
+    @FXML
+    private TableColumn<ProductModel, Double> priceCol;
+
+    @FXML
+    private TableColumn<ProductModel, Integer> amountCol;
 
     @FXML
     private TextField ProdID;
@@ -42,6 +74,8 @@ public class EmployeeController{
 
     @FXML
     private ContextMenu ProductTable;
+
+    ObservableList<ProductModel> olist = FXCollections.observableArrayList();
 
     @FXML
     public void FilterProductsOnAction(ActionEvent event) {
@@ -88,5 +122,42 @@ public class EmployeeController{
         Scene addScene = new Scene(parent);
         add.setScene(addScene);
         add.show();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        //Create DB Connection
+        try {
+            DBController ctr = new DBController();
+            Connection con = ctr.getConnection();
+            ResultSet rs = con.createStatement().executeQuery("select * from Product");
+
+            //Read rows
+            while(rs.next()){
+                olist.add(new ProductModel(rs.getInt("productID"),
+                        rs.getInt("categorie"),
+                        rs.getString("material"),
+                        rs.getDouble("price"),
+                        rs.getString("size"),
+                        rs.getString("describtion"),
+                        rs.getInt("amountAvailable")));
+            }
+            rs.close();
+            con.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        //Set table columns
+        colID.setCellValueFactory(new PropertyValueFactory<>("id"));
+        TypeCol.setCellValueFactory(new PropertyValueFactory<>("categorie"));
+        materialCol.setCellValueFactory(new PropertyValueFactory<>("material"));
+        sizeCol.setCellValueFactory(new PropertyValueFactory<>("size"));
+        descCol.setCellValueFactory(new PropertyValueFactory<>("description"));
+        priceCol.setCellValueFactory(new PropertyValueFactory<>("price"));
+        amountCol.setCellValueFactory(new PropertyValueFactory<>("amount"));
+
+        table.setItems(olist);
     }
 }
--- src/FurnitureStore/login/LoginController.java
+++ src/FurnitureStore/login/LoginController.java
@@ -1,11 +1,25 @@
 package FurnitureStore.login;
 
 import FurnitureStore.base.Controller;
+import FurnitureStore.base.DBController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
+import javafx.scene.control.*;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class LoginController {
+
+    @FXML
+    private TextField usernameTxt;
+
+    @FXML
+    private TextField passTxt;
+
+    @FXML
+    private Label loginLable;
 
     @FXML
     private Button loginButton;
@@ -14,7 +28,40 @@ public class LoginController {
         Controller.changeScene(event, "/FurnitureStore/signup/signup.fxml");
     }
 
-    public void LoginButtonOnAction(ActionEvent event) {
-        Controller.startEmployeeInterface(event, "/FurnitureStore/employee/EmployeeView.fxml");
+    public void LoginButtonOnAction(ActionEvent event) throws SQLException {
+
+        String user = usernameTxt.getText();
+        String pass = passTxt.getText();
+
+        //Conecting to DB
+        DBController connection = new DBController();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        //Create querry
+        String sql = "select password from EmployeeAccount where username = ?";
+        ps = connection.getConnection().prepareStatement(sql);
+        ps.setString(1, user);
+        rs = ps.executeQuery();
+
+
+        //Checking username/password
+        if(!rs.next()){
+            loginLable.setText("Username dont exist");
+            rs.close();
+            ps.close();
+            connection.getConnection().close();
+            return;
+        }
+        String realPass = rs.getString(1);
+
+        if(realPass.equals(pass)){
+            Controller.startEmployeeInterface(event, "/FurnitureStore/employee/EmployeeView.fxml");
+        }else{
+            loginLable.setText("Password not correct");
+        }
+        rs.close();
+        ps.close();
+        connection.getConnection().close();
     }
 }
