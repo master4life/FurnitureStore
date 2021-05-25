33292941fae0783ef67a50bdc6a2fd7c495f1ac2
marin <marinhincu1999@gmail.com>
2021-05-25 21:34:11 +0200

    Implemented Add Product Functionality

--- src/FurnitureStore/employee/AddProductController.java
+++ src/FurnitureStore/employee/AddProductController.java
@@ -1,5 +1,9 @@
 package FurnitureStore.employee;
 
+import FurnitureStore.base.DBController;
+import FurnitureStore.models.ProductModel;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -7,6 +11,10 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class AddProductController {
 
@@ -20,7 +28,7 @@ public class AddProductController {
     private ChoiceBox<?> Type1;
 
     @FXML
-    private ChoiceBox<?> Material1;
+    private ChoiceBox<String> material1;
 
     @FXML
     private TextField txtHeight1;
@@ -43,8 +51,38 @@ public class AddProductController {
     @FXML
     private Button btnAdd;
 
+    // TODO: Error Handling
     @FXML
     void addProduct(ActionEvent event) {
+        int productCategory;
+        if(rbtnAccessoire1.isSelected()){
+            productCategory = 1;
+        }else {
+            productCategory = 2;
+        }
 
+        String productMaterial = (String) material1.getSelectionModel().getSelectedItem();
+
+        double productPrice = Double.parseDouble(txtPrice1.getText().trim());
+
+        String productSize = txtHeight1.getText().trim() + "x" + txtLength1.getText().trim() + "x" + txtWidth1.getText().trim();
+
+        String productDescription = Description1.getText();
+
+        int productAmount = Integer.parseInt(txtAmount1.getText().trim());
+
+        ProductModel productToBeAdded = new ProductModel(productCategory, productMaterial, productPrice, productSize, productDescription, productAmount);
+        try {
+            DBController dbController = new DBController();
+            Connection connection = dbController.getConnection();
+            Statement statement = connection.createStatement();
+
+            String insertQuery = "insert into Product (categorie, material, price, size, describtion, amountAvailable)  values ("
+                                    + productToBeAdded.getCategorie()+",'"+productToBeAdded.getMaterial()+"',"+productToBeAdded.getPrice()+",'"
+                                    +productToBeAdded.getSize()+"','"+productToBeAdded.getDescription()+"',"+productToBeAdded.getAmount()+")";
+            statement.executeUpdate(insertQuery);
+        } catch (SQLException throwable) {
+            throwable.printStackTrace();
+        }
     }
 }
