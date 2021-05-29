55721f4216fc3dc2d45f950f889cc5b63582dc03
xhmxd <ahmed.noorzi@arcor.de>
2021-05-29 13:14:11 +0200

    Added customer views

---
+++ src/FurnitureStore/customer/ContactController.java
@@ -0,0 +1,17 @@
+package FurnitureStore.customer;
+
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.control.*;
+
+
+public class ContactController {
+
+    @FXML
+    private ContextMenu CustomerProductTable;
+
+    @FXML
+    public void MakeReservationOnAction(ActionEvent e){
+
+    }
+}
--- src/FurnitureStore/customer/CustomerController.java
+++ src/FurnitureStore/customer/CustomerController.java
@@ -1,21 +1,9 @@
 package FurnitureStore.customer;
 
-import FurnitureStore.base.Controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
-
 import java.io.IOException;
-import java.net.URL;
-import java.util.Objects;
-import java.util.ResourceBundle;
 
 public class CustomerController{
 
@@ -38,10 +26,7 @@ public class CustomerController{
     private TextField txtMaxPrice;
 
     @FXML
-    private Button FilterButton;
-
-    @FXML
-    private ContextMenu ProductTable;
+    private ContextMenu CustomerProductTable;
 
     @FXML
     public void FilterProductsOnAction(ActionEvent event) {
@@ -54,7 +39,7 @@ public class CustomerController{
     }
 
     @FXML
-    public void BuyButtonOnAction(ActionEvent event) throws IOException {
+    public void OrderButtonOnAction(ActionEvent event) throws IOException {
 
     }
 
