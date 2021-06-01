47a506b22e47e083ddc79cb9a1d8b8eb1c5006e5
alllexander1 <alexanderatanassov7@gmail.com>
2021-06-02 00:12:56 +0200

    Registration added and most of customer part

--- src/FurnitureStore/employee/EmployeeController.java
+++ src/FurnitureStore/employee/EmployeeController.java
@@ -140,6 +140,7 @@ public class EmployeeController implements Initializable{
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
+
         //Create DB Connection
         try {
             DBController ctr = new DBController();
--- src/FurnitureStore/login/LoginController.java
+++ src/FurnitureStore/login/LoginController.java
@@ -1,16 +1,29 @@
 package FurnitureStore.login;
 
+import FurnitureStore.CurrentUser;
 import FurnitureStore.base.Controller;
 import FurnitureStore.base.DBController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 
+import java.net.URL;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
-public class LoginController {
+public class LoginController implements Initializable {
+
+    @FXML
+    private RadioButton radioCustomer;
+
+    @FXML
+    private ToggleGroup group;
+
+    @FXML
+    private RadioButton radioEmployee;
 
     @FXML
     private TextField usernameTxt;
@@ -30,15 +43,32 @@ public class LoginController {
         String user = usernameTxt.getText();
         String pass = passTxt.getText();
 
+        String nextScene;
+        String sql;
+        String title;
+        int type;
+
+        if(radioCustomer.isSelected()){
+            sql = "select password from CustomerAccount where username = ?";
+            nextScene = "/FurnitureStore/customer/CustomerView.fxml";
+            title = "Furniture Shop";
+            type = 1;
+        }else{
+            sql = "select password from EmployeeAccount where username = ?";
+            nextScene = "/FurnitureStore/employee/EmployeeView.fxml";
+            title = "Products overview";
+            type = 2;
+        }
+
         //Connecting to DB
         DBController connection = new DBController();
         PreparedStatement ps = null;
         ResultSet rs = null;
 
-        //Create query
-        String sql = "select password from EmployeeAccount where username = ?";
+        //Create querry
         ps = connection.getConnection().prepareStatement(sql);
         ps.setString(1, user);
+       // ps.setString(2, user);
         rs = ps.executeQuery();
 
         //Checking username/password
@@ -52,12 +82,19 @@ public class LoginController {
         String realPass = rs.getString(1);
 
         if(realPass.equals(pass)){
-            Controller.changeScene(event, "/FurnitureStore/employee/EmployeeView.fxml");
+            CurrentUser.setUsername(user);
+            CurrentUser.setType(type);
+            Controller.startUserInterface(event, nextScene, title);
         }else{
             loginLabel.setText("Password not correct");
         }
         rs.close();
         ps.close();
         connection.getConnection().close();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        radioCustomer.setSelected(true);
     }
 }
